a26e9b993520d05c3fa48fb236d8434d00c50865
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-28 11:53:08 +0200

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,19 +19,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    # assert_not_empty cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
 
-    # 1. Visit the login path.
-    # 2. Verify that the new sessions form renders properly.
-    # 3. Post to the sessions path with an invalid params hash.
-    # 4. Verify that the new sessions form gets re-rendered and that a flash message appears.
-    # 5. Visit another page (such as the Home page).
-    # 6. Verify that the flash message doesn’t appear on the new page.
-
+  test "login without remembering" do
+    # Log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 
 end
